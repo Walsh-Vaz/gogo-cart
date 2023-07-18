9070d886d9ee1a6d64da1249ed75615328d4d25d
Jackie Tan <jackilyn.tan@gmail.com>
2023-07-18 01:09:38 -0400

    server js cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './styles.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// reportWebVitals();
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 reportWebVitals();
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
